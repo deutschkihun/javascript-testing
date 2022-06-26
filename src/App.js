3f811b91303a6148b89901713fa0e35844003973
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2022-06-26 17:31:41 +0900

    add testing basic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 2;
+  const b = 4;
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          data-testid="customId"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,6 +20,13 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          <li>apple</li>
+          <li>peach</li>
+          <li>watermelon</li>
+        </ul>
+        <h1 data-testid="title">hello</h1>
+        <span title="sum">{a+b}</span>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,47 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react/i); // handling upper and lower case of alphabet
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link with sensitive text', () => {
+  render(<App />);
+  const linkElement = screen.getByText("Learn React"); // ex.) learn react (fail)
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders learn react link with testid', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('customId')
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders 3 list items', () => {
+  render(<App />);
+  const listItem = screen.getAllByRole('listitem')
+  expect(listItem).toHaveLength(3)
+  expect(listItem.length).toBe(3)
+  expect(listItem.length).toEqual(3)
+});
+
+test('render title', () => {
+  render(<App/>)
+  const title = screen.getByTestId('title');
+  expect(title).toBeInTheDocument()
+})
+
+test('sum should be 6', () => {
+  render(<App/>)
+  const sum = screen.getByTitle('sum');
+  expect(sum.textContent).toBe('6')
+})
+
+// testing library
+// to test our application we need to reach out dom element first and to reach all those elements
+// we are using testing library by using many different queries and also interact with dom element
+
+// jest
+// javascript testing library and it allows us to run our test and it determines whether the tests pass 
+// or fail, it takes our expectation and compare with the actual result and it's called an assertion
